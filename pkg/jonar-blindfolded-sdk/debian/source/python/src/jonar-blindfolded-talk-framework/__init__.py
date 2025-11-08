4485be98d16d35af110c130c376fb97484bcc100
SDKTools <jonaslan495@gmail.com>
2025-11-08 19:17:04 -0300

    JBTInterface Development Start Commit #3

--- pkg/jonar-blindfolded-sdk/debian/source/python/src/jonar-blindfolded-talk-framework/__init__.py
+++
@@ -1,9 +0,0 @@
-def get_version():
-    return (25, 11)
-
-__version__ = '.'.join(map(str, get_version()))
-__name__ = "jonar-blindfolded-talk-framework"
-__author__ = "Jonas Andrade Neves Drummond de Paiva"
-__author_email__ = "locdev.loss.of.control@gmail.com"
-__description__ = "A talk framework for Jonar Blindfolded project."
-__license__ = "GPLv2+"
